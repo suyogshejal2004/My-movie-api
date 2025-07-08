829ae5014852a80463df9cc537a307e9ce345eff
suyogshejal2004 <161924832+suyogshejal2004@users.noreply.github.com>
2025-07-08 16:47:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // <-- required for Render
 
 // Load movie data from movies.json
 const movies = JSON.parse(fs.readFileSync('movies.json', 'utf-8'));
